f41d30d5c3abcda60c06552362ecf2956b7a2fcd
Edu <edu@edu4java.com>
2023-01-21 15:36:51 +0100

    Podcast functionality added

--- src/views/Podcast.jsx
+++ src/views/Podcast.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './Podcast.css'
+import { Link, useParams } from "react-router-dom"
+import { getPodcasts, getEpisodes } from '../services/podcasts'
+import Sidebar from '../components/Sidebar'
+
+const milliseconds2Minutes = ms => {
+  let seconds = ((ms % 60000) / 1000).toFixed(0)
+  return Math.floor(ms / 60000) + ":" + (seconds < 10 ? '0' : '') + seconds
+}
+
+const fromatDate = d => `${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`
 
 export const Podcast = () => {
+  let { idPodcast } = useParams()
+  const [podcast, setPodcast] = useState([])
+  const [episodes, setEpisodes] = useState([])
+  useEffect(() => {
+    getPodcasts().then(r => { setPodcast(r.filter(p => p.id == idPodcast)[0]) })
+    getEpisodes(idPodcast).then(r => { setEpisodes(r) })
+  }, [])
   return (
-    <div>Podcast</div>
+    <div id="container">
+      <Sidebar name={podcast.name} img={podcast.img} author={podcast.author} summary={podcast.summary} />
+      <div id="main-content">
+        <div id="title">
+          <h1>Episodes: {episodes.length}</h1>
+        </div>
+        <div id="list-container">
+          <table>
+            <thead>
+              <tr> <th>Title</th> <th>Date</th> <th>Duration</th> </tr>
+            </thead>
+            <tbody>
+              {episodes.map((item, index) => (
+                <tr key={index} >
+                  <td className="td-title">
+                    <Link to={`/podcast/${idPodcast}/episode/${item.id}`}> {item.name} </Link>
+                  </td>
+                  <td>{fromatDate(new Date(item.date))} </td>
+                  <td>{milliseconds2Minutes(item.ms)} </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   )
 }
+
