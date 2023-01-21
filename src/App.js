386c5c5fd5fa32873ef499c73e4f29e02354ba3c
Edu <edu@edu4java.com>
2023-01-21 14:21:34 +0100

    router and views added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link, } from "react-router-dom";
+import { Home } from './views/Home';
+import { Podcast } from './views/Podcast';
+import { Episode } from './views/Episode';
 
 function App() {
   return (
-    <div className="App">
-      Hello edu4java
-    </div>
+      <Router>
+      <h1>
+        <Link to={`/`}>Podcaster</Link> 
+      </h1>
+      <hr/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="podcast/:idPodcast" element={<Podcast />} />
+          <Route path="podcast/:idPodcast/episode/:idEpisode" element={<Episode />} />
+        </Routes>
+      </Router>
   );
 }
 
