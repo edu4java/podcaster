96aed66ab2033a3fe41493bf0126da45b2815100
Edu <edu@edu4java.com>
2023-01-21 15:23:26 +0100

    Episodes fetch added to service

--- src/services/podcasts.js
+++ src/services/podcasts.js
@@ -4,3 +4,9 @@ export const getPodcasts = async () =>
             id: x.id.attributes["im:id"], name: x["im:name"].label, author: x["im:artist"].label,
             img: x["im:image"][[x["im:image"].length - 1]].label, summary: x.summary.label
         }))
+
+export const getEpisodes = async id =>
+    (await (await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=podcastEpisode&limit=100`)).json())
+        .results.filter((_, i) => i > 0).map(x => ({
+            name: x.trackName, date: x.releaseDate, ms: x.trackTimeMillis, id: x.episodeGuid
+        }))
