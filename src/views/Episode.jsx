7f3f4e52026c4bbd6d791e2ed3f56f7be9f672af
Edu <edu@edu4java.com>
2023-01-21 19:32:11 +0100

    Episode view functionaliy. TODO streaming player

--- src/views/Episode.jsx
+++ src/views/Episode.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import './Episode.css'
+import { useParams } from "react-router-dom"
+import { getPodcasts, getEpisodes } from '../services/podcasts'
+import Sidebar from '../components/Sidebar'
 
 export const Episode = () => {
+  let { idPodcast, idEpisode } = useParams()
+  const [podcast, setPodcast] = useState([])
+  const [episode, setEpisode] = useState([])
+
+  useEffect(() => {
+    getPodcasts().then(r => { setPodcast(r.filter(p => p.id == idPodcast)[0]) })
+    getEpisodes(idPodcast).then(r => { setEpisode(r.filter(e => e.id == idEpisode)[0]) })
+  }, [])
+
   return (
-    <div>Episode</div>
+    <div id="container">
+      <Sidebar name={podcast.name} img={podcast.img} author={podcast.author} summary={podcast.summary} />
+      <div id="main-container">
+        <h4>{episode.name}</h4>
+        <p className='it'> {episode.desc}</p>
+        <div id="podcast-player">
+          <audio src={episode.mp3} controls> </audio>
+        </div>
+      </div>
+    </div>
   )
+
+
 }
